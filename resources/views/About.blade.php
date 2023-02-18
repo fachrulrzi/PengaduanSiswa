ae9e7776189f9d765bb832b68df5f06019714056
fachrulrzi <fachrulrozi705@gmail.com>
2023-02-18 20:46:07 +0700

    first commit

---
+++ resources/views/About.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+@section('container')
+<div class="row">
+    <div class="col-12 mb-4">
+        <img src="{{ asset('img/a5f0e031592e11b0d022e8a963140a43.jpg') }}" class="d-block rounded-3" height="600" width="100%"
+                  alt="...">
+    </div>
+    <div class="col-12">
+            <Article>
+                <h1 class="fw-bold mb-2">Layanan Pengaduan Masyarakat</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab facilis in est optio fugiat recusandae illum quasi vel reprehenderit, esse atque odio nostrum minima voluptatibus molestias nam id velit debitis praesentium eveniet eaque laboriosam. t vero qui soluta doloremque vitae animi consequuntur nulla, molestiae fugit nemo dolorum non voluptas.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam cum earum quidem? Ullam quibusdam sequi cupiditate laudantium hic natus atque et nulla id, illo necessitatibus? Porro minima sapiente aliquam quam quod accusamus ad nisi, qui maiores nobis molestiae provident nesciunt natus veniam nisi quae quam fugit esse est animi. Ullam, aperiam rem animi blanditiis nobis optio, adipisci soluta ducimus aut labore ratione, quae dolores suscipit sint ipsum provident.</p>
+            </Article>
+      
+    </div>
+</div>
+@endsection
